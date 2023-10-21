642e3eee9809e2f34012369e96def8333eba092c
AshlavaDev <104705344+AshlavaDev@users.noreply.github.com>
2023-10-20 20:54:58 -0600

    add modal to services page

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,9 +1,25 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
 import Cleaning from "@/components/sections/services/Cleaning";
 import Security from "@/components/sections/services/Security";
 import PR from "@/components/sections/services/PR";
+import Modal from "@/components/UI/Modal";
 
-//TODO: Add actual content
 export default function ServicePage() {
+  const [loadModal, setLoadModal] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoadModal(true);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
   return (
     <main className="flex flex-col gap-16 px-4 pb-8 pt-20 text-cyberyellow md:gap-28 md:px-10 md:pb-20 md:pt-32">
       <h1 className="pt-8 text-center font-heading text-4xl font-bold md:text-6xl">
@@ -13,6 +29,11 @@ export default function ServicePage() {
       <Cleaning />
       <Security />
       <PR />
+      {loadModal && (
+        <Modal onClose={() => setLoadModal(false)}>
+          <p>This simple page is used to showcase the &quot;services&quot; of Kyber. While liek the company none of the services are real, they allowed me to be a little creative while also allowing me to practice with some Framer Motion animations.</p>
+        </Modal>
+      )}
     </main>
   );
 }
