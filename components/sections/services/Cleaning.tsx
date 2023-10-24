3b08b09d65896752e33d8ff7e36ed80be5f682f3
AshlavaDev <104705344+AshlavaDev@users.noreply.github.com>
2023-10-24 16:10:39 -0600

    update service section components to use useEffect and useState to fix build issues, even though it worked despite them

--- components/sections/services/Cleaning.tsx
+++ components/sections/services/Cleaning.tsx
@@ -2,15 +2,34 @@
 
 import { motion } from "framer-motion";
 import Image from "next/image";
+import { useState, useEffect } from "react";
 
 export default function Cleaning() {
+  const [viewport, setViewport] = useState<number | "all" | "some" | undefined>(
+    "some",
+  );
+
+  useEffect(() => {
+    const handleResize = () => {
+      setViewport(document.documentElement.clientWidth > 768 ? "all" : "some");
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <motion.section
       className="flex flex-col items-center justify-center opacity-0"
       id="cleaning"
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
-      viewport={{ amount: window.innerWidth > 768 ? "all" : "some" }}
+      viewport={{ amount: viewport }}
     >
       <div className="flex flex-col items-center gap-8 md:flex-row md:gap-24">
         <div className="h-full w-full max-w-prose flex-1 items-center justify-center border border-cyberyellow">
